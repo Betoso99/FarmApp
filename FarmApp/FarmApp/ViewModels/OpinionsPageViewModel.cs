2ea5d9b9299e32f217893ec7efea453b5eb119f5
Betoso99 <55248968+Betoso99@users.noreply.github.com>
2021-01-04 01:20:17 -0400

    Store pages

--- FarmApp/FarmApp/ViewModels/OpinionsPageViewModel.cs
+++ FarmApp/FarmApp/ViewModels/OpinionsPageViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Prism.Commands;
+﻿using FarmApp.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace FarmApp.ViewModels
@@ -10,6 +12,7 @@ namespace FarmApp.ViewModels
     public class OpinionsPageViewModel : ViewModelBase
     {
         public INavigationService _navigationService { get; set; }
+        public ObservableCollection<Opinions> Opinions { get; set; }
 
         public OpinionsPageViewModel(INavigationService navigationService) :
             base(navigationService)
--- FarmApp/FarmApp/ViewModels/SharePageViewModel.cs
+++ FarmApp/FarmApp/ViewModels/SharePageViewModel.cs
@@ -4,6 +4,10 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Xamarin.Essentials;
+using System.Windows.Input;
+using System.Threading.Tasks;
+using Prism.Services;
 
 namespace FarmApp.ViewModels
 {
@@ -11,12 +15,31 @@ namespace FarmApp.ViewModels
     public class SharePageViewModel : ViewModelBase
     {
         public INavigationService _navigationService { get; set; }
+        public IPageDialogService _dialogService { get; set; }
+        public DelegateCommand CopyCommand { get; set; }
+        public string Link { get; set; }
 
-        public SharePageViewModel(INavigationService navigationService) :
+        public SharePageViewModel(INavigationService navigationService, IPageDialogService dialogService) :
             base(navigationService)
         {
             Title = "Share";
+            _dialogService = dialogService;
             _navigationService = navigationService;
+            Link = GetLink();
+
+            CopyCommand = new DelegateCommand(async () => await Copy());
+        }
+
+        async Task Copy()
+		{
+            await Clipboard.SetTextAsync(Link);
+            await _dialogService.DisplayAlertAsync("Done", "Your Link has been succesfully copied", "OK"); ;
+        }
+
+        public String GetLink()
+        {
+            Link = "Hello World";
+            return Link;
         }
     }
 }
